b69787641d0e6282f0dba82139df662a490e0151
Jocelyn Pender <jpend049@uottawa.ca>
2020-07-10 16:30:54 -0400

    parallel plot of full dataset

--- viz/parallel_plot.R
+++ viz/parallel_plot.R
@@ -4,6 +4,8 @@ library(GGally)
 
 gbif_us_dataset <- read_csv("gbif_institutionCode_summmary/gbif_us_institutionCode_MIDS_2020.07.08.csv")
 
+############# TOP 5 #############
+
 # Filter down to the top 5 institutions
 gbif_us_dataset <- gbif_us_dataset %>% top_n(5, total)
 
@@ -44,7 +46,7 @@ generate_ggally_parcoord_plot <- function(gbif_us_dataset) {
 ggally_parcordd_plot <- generate_ggally_parcoord_plot(gbif_us_dataset)
 ggally_parcordd_plot
 
-############# BOTTOM 5 #################
+############# BOTTOM 5 #############
 
 gbif_us_dataset <- read_csv("gbif_institutionCode_summmary/gbif_us_institutionCode_MIDS_2020.07.08.csv")
 
@@ -58,4 +60,30 @@ mass_parcoord_plot
 
 ggally_parcordd_plot <- generate_ggally_parcoord_plot(gbif_us_dataset)
 ggally_parcordd_plot
+
+
+############# FULL DATASET #############
+
+gbif_us_dataset <- read_csv("gbif_institutionCode_summmary/gbif_us_institutionCode_MIDS_2020.07.08.csv")
+
+generate_ggally_parcoord_plot <- function(gbif_us_dataset) {
+  # Sort columns based on MIDS level
+  gbif_us_dataset <- gbif_us_dataset %>% relocate(has_collectionCode, has_catalogNumber, has_countryCode, has_higherGeography, has_locality, has_eventDate, has_dateIdentified, has_identifiedBy, has_recordedBy, has_image, has_coordinates, has_speciesKey)
+  
+  # Generate percentage of totals as data
+  percentage_gbif_us_dataset <- gbif_us_dataset[, 1:12] %>%
+    mutate(across(everything()), . / gbif_us_dataset$total) 
+  
+  # add back in the institutionCode
+  percentage_gbif_us_dataset <- cbind(percentage_gbif_us_dataset, gbif_us_dataset$institutionCode)
+  colnames(percentage_gbif_us_dataset)[13] <- "institutionCode"
+  parcordd_plot <- ggparcoord(data = percentage_gbif_us_dataset, columns = 1:12, scale = "uniminmax", alphaLines = 0.05) + theme_bw()
+  return(parcordd_plot)
+}
+
+ggally_parcordd_plot <- generate_ggally_parcoord_plot(gbif_us_dataset)
+ggally_parcordd_plot
+
+
+
 
