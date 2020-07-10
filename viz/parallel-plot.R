fbaa458482ecd1858740ea5f7d2523b20465b020
Jocelyn Pender <jpend049@uottawa.ca>
2020-07-10 10:18:02 -0400

    filter plot to top_n

--- viz/parallel-plot.R
+++ viz/parallel-plot.R
@@ -3,6 +3,11 @@ library(tidyverse)
 
 gbif_us_dataset <- read_csv("gbif_institutionCode_summmary/gbif_us_institutionCode_MIDS_2020.07.08.csv")
 
+# Filter down to the top 5 institutions
+gbif_us_dataset <- gbif_us_dataset %>% top_n(5, total)
+
+
+
 # Generate percentage of totals as data
 percentage_gbif_us_dataset <- gbif_us_dataset[, 3:16] %>%
   mutate(across(everything()), . / gbif_us_dataset$total) 
@@ -17,3 +22,4 @@ my_colors <- colors()[numeric_institutions*11]
 
 # Make the graph !
 parcoord(percentage_gbif_us_dataset, col = my_colors)
+
