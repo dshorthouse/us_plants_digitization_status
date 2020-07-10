0c80e8d5974c9b49fac0bda49e20d6f4fd734917
Jocelyn Pender <jpend049@uottawa.ca>
2020-07-10 10:11:22 -0400

    plot percentages of total and remove meaningless columns

--- viz/parallel-plot.R
+++ viz/parallel-plot.R
@@ -3,17 +3,17 @@ library(tidyverse)
 
 gbif_us_dataset <- read_csv("gbif_institutionCode_summmary/gbif_us_institutionCode_MIDS_2020.07.08.csv")
 
-#generate_column_prop <- function(dataset, col_name) {
+# Generate percentage of totals as data
+percentage_gbif_us_dataset <- gbif_us_dataset[, 3:16] %>%
+  mutate(across(everything()), . / gbif_us_dataset$total) 
   
-#}
-
-#gbif_us_dataset <- gbif_us_dataset %>%
-#  select(has_image, total) %>%
-#  mutate(image_prop = has_image / total, .keep = "all")
+# Remove has_scientificName and has_acceptedNameUsage
+drop_cols <- c("has_scientificName", "has_acceptedNameUsage")
+percentage_gbif_us_dataset <- percentage_gbif_us_dataset %>% select(-one_of(drop_cols))
 
 # Vector color
 numeric_institutions <- gbif_us_dataset$institutionCode %>% as.factor() %>% as.numeric()
 my_colors <- colors()[numeric_institutions*11]
 
 # Make the graph !
-parcoord(gbif_us_dataset[,c(2:5)], col = my_colors)
+parcoord(percentage_gbif_us_dataset, col = my_colors)
