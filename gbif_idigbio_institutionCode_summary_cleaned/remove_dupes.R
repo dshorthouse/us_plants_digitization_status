2742d7422c8c2d67cd65dda957132fb82f6e2e9a
Jocelyn Pender <jpend049@uottawa.ca>
2020-10-14 12:02:07 -0400

    duplicates aggregate and excel file for october dataset

--- gbif_idigbio_institutionCode_summary_cleaned/remove_dupes.R
+++ gbif_idigbio_institutionCode_summary_cleaned/remove_dupes.R
@@ -1,19 +1,19 @@
 library(tidyverse)
 
-aggregate_counts_sum_return_new_dataset <- function(dataset) {
-  file_name <- dataset$File[1]
-  dataset <- dataset %>% select(-File) %>% group_by(institutionCode) %>% summarise_all(funs(sum))
-  File <- rep(file_name,nrow(dataset))
+aggregate_counts_sum_return_new_dataset <- function(file_name) {
+  dataset <- read_csv(file_name)
+  dataset <- dataset %>% group_by(institutionCode) %>% summarise_all(funs(sum))
+  File <- rep(file_name, nrow(dataset))
   dataset <- cbind(dataset, File)
-  return(dataset)
+  write_csv(dataset, paste(strsplit(file_name, ".csv")[[1]][1], "agg.csv", sep="_"))
 }
 
 # remove duplicates
-idigbio_us_dataset <- read_csv("cleaned_idigbio_us_institutionCode_MIDS_2020-07-13-csv.csv")
-idigbio_us_dataset <- aggregate_counts_sum_return_new_dataset(idigbio_us_dataset)
-write_csv(idigbio_us_dataset, "cleaned_idigbio_us_institutionCode_MIDS_2020-07-13-csv_agg.csv")
+# idigbio_us_dataset <- read_csv("cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv.csv")
+idigbio_us_dataset <- aggregate_counts_sum_return_new_dataset("cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv.csv")
+# write_csv(idigbio_us_dataset, "cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv_agg.csv")
 
 # import the GBIF data dump
-gbif_us_dataset <- read_csv("cleaned_gbif_us_institutionCode_MIDS_2020.07.08.csv")
-gbif_us_dataset <- aggregate_counts_sum_return_new_dataset(gbif_us_dataset)
-write_csv(gbif_us_dataset, "cleaned_gbif_us_institutionCode_MIDS_2020.07.08_agg.csv")
+# gbif_us_dataset <- read_csv("cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv.csv")
+gbif_us_dataset <- aggregate_counts_sum_return_new_dataset("cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv.csv")
+# write_csv(gbif_us_dataset, "cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv_agg.csv")
