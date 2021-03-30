bc2801a0b7d4213aec75ab80b1bf552c58891371
Jocelyn Pender <jpend049@uottawa.ca>
2021-03-30 11:53:24 -0400

    aggregate duplicated institution code data

--- gbif_idigbio_institutionCode_summary_cleaned/src/merge_pulls.R
+++ gbif_idigbio_institutionCode_summary_cleaned/src/merge_pulls.R
@@ -1,22 +1,25 @@
 library(tidyverse)
 
-# combine data from the two pulls, july and october
+# combine data from the three pulls, july, october, december
 
 
-idig_oct <- read_csv("../agg_month/cleaned_idigbio_us_institutionCode_MIDS_2020-10-09_agg.csv")
-gbif_oct <- read_csv("../agg_month/cleaned_gbif_us_institutionCode_MIDS_2020-10-09_agg.csv")
+idig_oct <- read_csv("../agg_csv/cleaned_idigbio_us_institutionCode_MIDS_2020-10-09_agg.csv")
+gbif_oct <- read_csv("../agg_csv/cleaned_gbif_us_institutionCode_MIDS_2020-10-09_agg.csv")
 
-idig_july <- read_csv("../agg_month/cleaned_idigbio_us_institutionCode_MIDS_2020-07-13-csv_agg.csv")
-gbif_july <- read_csv("../agg_month/cleaned_gbif_us_institutionCode_MIDS_2020.07.08_agg.csv")
+idig_july <- read_csv("../agg_csv/cleaned_idigbio_us_institutionCode_MIDS_2020-07-13-csv_agg.csv")
+gbif_july <- read_csv("../agg_csv/cleaned_gbif_us_institutionCode_MIDS_2020.07.08_agg.csv")
+
+idig_dec <- read_csv("../agg_csv/cleaned_idigbio_us_institutionCode_MIDS_2020-12-15-csv_agg.csv")
+gbif_dec <- read_csv("../agg_csv/cleaned_gbif_us_institutionCode_MIDS_2020-12-06-csv_agg.csv")
 
 # make one csv for idig
-idig <- bind_rows(list(idig_oct, idig_july))
-idig$month <- recode(idig$File, "idigbio_us_institutionCode_MIDS_2020.07.13.csv" = "july", "cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv.csv" = "october")
+idig <- bind_rows(list(idig_dec, idig_oct, idig_july))
+idig$month <- recode(idig$File, "idigbio_us_institutionCode_MIDS_2020.07.13.csv" = "july", "cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv.csv" = "october", "../agg_month/cleaned_idigbio_us_institutionCode_MIDS_2020-12-15-csv_agg.csv" = "december")
 write_csv(idig, "../agg_month_csv/cleaned_idigbio_us_institutionCode_MIDS.csv")
 
 # one csv for gbif
-gbif <- bind_rows(list(gbif_oct, gbif_july))
-gbif$month <- recode(gbif$File, "gbif_us_institutionCode_MIDS_2020.07.08.csv" = "july", "cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv.csv" = "october")
+gbif <- bind_rows(list(gbif_oct, gbif_july, gbif_dec))
+gbif$month <- recode(gbif$File, "gbif_us_institutionCode_MIDS_2020.07.08.csv" = "july", "cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv.csv" = "october", "../agg_month/cleaned_gbif_us_institutionCode_MIDS_2020-12-06-csv_agg.csv" = "december")
 write_csv(gbif, "../agg_month_csv/cleaned_gbif_us_institutionCode_MIDS.csv")
 
 
@@ -29,5 +32,8 @@ oct$month <- rep("october", nrow(oct))
 july <- idig_july %>% full_join(gbif_july, by = "institutionCode", suffix = c("_idig", "_gbif"))
 july$month <- rep("july", nrow(july))
 
-full_dataset <- bind_rows(list(oct, july))
+dec <- idig_dec %>% full_join(gbif_dec, by = "institutionCode", suffix = c("_idig", "_gbif"))
+dec$month <- rep("december", nrow(dec))
+
+full_dataset <- bind_rows(list(dec, oct, july))
 write_csv(full_dataset, "../excel_files_for_tableau/full_dataset.csv")
--- gbif_idigbio_institutionCode_summary_cleaned/src/remove_dupes.R
+++ gbif_idigbio_institutionCode_summary_cleaned/src/remove_dupes.R
@@ -1,19 +1,22 @@
 library(tidyverse)
 
 aggregate_counts_sum_return_new_dataset <- function(file_name) {
+  setwd("~/us_plants_digitization_status/gbif_idigbio_institutionCode_summary_cleaned/non_agg_csv")
   dataset <- read_csv(file_name)
   dataset <- dataset %>% group_by(institutionCode) %>% summarise_all(funs(sum))
   File <- rep(file_name, nrow(dataset))
   dataset <- cbind(dataset, File)
-  write_csv(dataset, paste(strsplit(file_name, ".csv")[[1]][1], "agg.csv", sep="_"))
+  setwd("../agg_csv")
+  write_csv(dataset, paste(strsplit(file_name, ".csv")[[1]][1], "csv_agg.csv", sep="-"))
 }
 
+
 # remove duplicates
-# idigbio_us_dataset <- read_csv("cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv.csv")
-idigbio_us_dataset <- aggregate_counts_sum_return_new_dataset("cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv.csv")
-# write_csv(idigbio_us_dataset, "cleaned_idigbio_us_institutionCode_MIDS_2020-10-09-csv_agg.csv")
+# idigbio_us_dataset <- read_csv("cleaned_idigbio_us_institutionCode_MIDS_2020-12-15-csv.csv")
+idigbio_us_dataset <- aggregate_counts_sum_return_new_dataset("cleaned_idigbio_us_institutionCode_MIDS_2020-12-15-csv.csv")
+# write_csv(idigbio_us_dataset, "cleaned_idigbio_us_institutionCode_MIDS_2020-12-15-csv_agg.csv")
 
 # import the GBIF data dump
-# gbif_us_dataset <- read_csv("cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv.csv")
-gbif_us_dataset <- aggregate_counts_sum_return_new_dataset("cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv.csv")
-# write_csv(gbif_us_dataset, "cleaned_gbif_us_institutionCode_MIDS_2020-10-09-csv_agg.csv")
+# gbif_us_dataset <- read_csv("cleaned_gbif_us_institutionCode_MIDS_2020-12-06-csv.csv")
+gbif_us_dataset <- aggregate_counts_sum_return_new_dataset("cleaned_gbif_us_institutionCode_MIDS_2020-12-06-csv.csv")
+# write_csv(gbif_us_dataset, "cleaned_gbif_us_institutionCode_MIDS_2020-12-06-csv_agg.csv")
