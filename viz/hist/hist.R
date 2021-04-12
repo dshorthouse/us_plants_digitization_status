7dd7a5004bd606962ebbd24c8075c53515e91bbf
Jocelyn Pender <jpend049@uottawa.ca>
2021-04-12 16:16:23 -0400

    fix hasitly constructed histogram looking at changes over time

--- viz/hist/hist.R
+++ viz/hist/hist.R
@@ -1,6 +1,8 @@
 
 full_dataset <- read_csv("gbif_idigbio_institutionCode_summary_cleaned/excel_files_for_tableau/full_dataset.csv")
 
+## iDigBio
+
 idigbio_changes <- full_dataset %>% group_by(institutionCode) %>% count(total_idig)
 idigbio_changes <- idigbio_changes %>% drop_na
 idigbio_changes <- idigbio_changes %>% rename(total_count_observed_num_times = n)
@@ -20,15 +22,28 @@ p <- ggplot(idigbio_num_changes_per_institution,
        y="Number of institutions", title = "Changes in iDigBio data")
 p
 
-table(idigbio_mods$nn)
-table(idigbio_mods$nn) / length(idigbio_mods$nn)
+table(idigbio_num_changes_per_institution$num_count_changes_per_institution)
+table(idigbio_num_changes_per_institution$num_count_changes_per_institution) / length(idigbio_num_changes_per_institution$num_count_changes_per_institution)
 
+## GBIF
 
 gbif_changes <- full_dataset %>% group_by(institutionCode) %>% count(total_gbif)
 gbif_changes <- gbif_changes %>% drop_na
+gbif_changes <- gbif_changes %>% rename(total_count_observed_num_times = n)
+
 write_csv(gbif_changes, "gbif_changes.csv")
 gbif_changes <- read_csv("gbif_changes.csv")
-gbif_mods <- gbif_changes %>% count(institutionCode, n)
-hist(gbif_mods$nn)
-table(gbif_mods$nn)
-table(gbif_mods$nn) / length(gbif_mods$nn)
+
+
+gbif_num_changes_per_institution <- gbif_changes %>% count(institutionCode)
+gbif_num_changes_per_institution <- gbif_num_changes_per_institution %>% rename(num_count_changes_per_institution = n)
+
+p <- ggplot(gbif_num_changes_per_institution, 
+            aes(x=num_count_changes_per_institution)) + 
+  geom_histogram(binwidth=1, color="black", fill="lightblue") + 
+  labs(x="Number of changes in the total number of specimens per institution over three time points", 
+       y="Number of institutions", title = "Changes in GBIF data")
+p
+
+table(gbif_num_changes_per_institution$num_count_changes_per_institution)
+table(gbif_num_changes_per_institution$num_count_changes_per_institution) / length(gbif_num_changes_per_institution$num_count_changes_per_institution)
